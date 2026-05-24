cada0a52ce3330d7f7639bcdcdba81a0f4889079
Burgentie <63803079+Burgentie@users.noreply.github.com>
2026-05-24 19:18:38 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Made with ❤️ The official The Lost & Found discord bot!",
         // Activity type number (0 = Playing).
         type: 0, 
       },
